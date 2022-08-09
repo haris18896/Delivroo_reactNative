f3b375a165d4b72447f836a65fa7cba1c6a71f30
haris.ahmad2 <haris.ahmad@sparkai.ca>
2022-08-09 16:32:05 +0500

    :sparkles: adding Delivery Screen
    last screen

--- screens/delivery/DeliveryScreen.js
+++ screens/delivery/DeliveryScreen.js
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import * as Icons from 'react-native-heroicons/solid'
+import { View, Text, SafeAreaView } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { selectRestaurant } from '../../redux/features/restaurantSlice'
+import { TouchableOpacity } from 'react-native-web'
 
 const DeliveryScreen = () => {
+  const navigation = useNavigation()
+  const restaurant = useSelector(selectRestaurant)
+
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
+    <View className='bg-[#00ccbb]'>
+      <SafeAreaView>
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+            <Icons.XIcon color='white' size={30} />
+          </TouchableOpacity>
+          <Text>Order Help</Text>
+        </View>
+      </SafeAreaView>
     </View>
   )
 }
