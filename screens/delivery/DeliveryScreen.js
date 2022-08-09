1eae5dfa4414a70400ce93112305e0466bb467f2
haris.ahmad2 <haris.ahmad@sparkai.ca>
2022-08-09 16:33:37 +0500

    :construction: adding
    deliveryScreen

--- screens/delivery/DeliveryScreen.js
+++ screens/delivery/DeliveryScreen.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import * as Icons from 'react-native-heroicons/solid'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { selectRestaurant } from '../../redux/features/restaurantSlice'
-import { TouchableOpacity } from 'react-native-web'
 
 const DeliveryScreen = () => {
   const navigation = useNavigation()
